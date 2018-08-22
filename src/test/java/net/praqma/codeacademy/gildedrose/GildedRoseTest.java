193d98ffc37f2a9f157d63f3e6b1f27a05f9b092
zlepper <hansen13579@gmail.com>
2018-08-22 09:41:32 +0200

    actually fix the test

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -8,12 +8,10 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        // This is a test
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        // assertEquals("fixme", app.items[0].name);
-        assertEquals("fixme", "fixme");
+        assertEquals("foo", app.items[0].name);
     }
 
 }
