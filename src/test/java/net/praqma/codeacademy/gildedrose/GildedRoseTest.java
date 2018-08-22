46ec9e654bed496e82d31c2187ebafddb672dd49
zlepper <hansen13579@gmail.com>
2018-08-22 09:53:00 +0200

    Actual test for thing

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -2,16 +2,33 @@ package net.praqma.codeacademy.gildedrose;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class GildedRoseTest {
+    GildedRose app;
+
+    @Before
+    public void initialize() {
+        Item[] items = new Item[] { new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                // this conjured item does not work properly yet
+                new Item("Conjured Mana Cake", 3, 6) };
+
+        app = new GildedRose(items);
+    }
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+        assertEquals(app.items[0].sellIn, 10);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        assertEquals(app.items[0].sellIn, 9);
     }
 
 }
